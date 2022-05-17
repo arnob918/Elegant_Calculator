a8389c5fc23bea6a2cfb1a157af5b3dc2ed29977
arnob918 <64248431+arnob918@users.noreply.github.com>
2022-05-17 17:10:31 +0600

    new java file created on FirstBranch named ArnobClass

---
+++ app/src/main/java/com/example/new_sample/ArnobClass.java
@@ -0,0 +1,17 @@
+package com.example.new_sample;
+
+public class ArnobClass {
+    int b;
+
+    public ArnobClass(int b) {
+        this.b = b;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
