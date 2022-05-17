0aa1706a2d5359de0bfd0981dfccdbd09bdc0686
arnob918 <64248431+arnob918@users.noreply.github.com>
2022-05-17 17:06:02 +0600

    new java file created in master branch named ArnobClass

---
+++ app/src/main/java/com/example/new_sample/ArnobClass.java
@@ -0,0 +1,17 @@
+package com.example.new_sample;
+
+public class ArnobClass {
+    int a;
+
+    public ArnobClass(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
